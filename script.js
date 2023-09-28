37c254f2732ef485da7f73e4df43c4c0ca50b674
Spencer Thayer <me@spencerthayer.com>
2023-09-28 12:40:13 -0700

    [feat] Add GA4, GTM, and Video Player scripts
    Added the necessary scripts for Google Analytics 4 (GA4), Google Tag Manager (GTM), and a video player. The GA4 script is asynchronously loaded from "https://www.googletagmanager.com/gtag/js?id=G-VH41JYZ1B0" and configured with the provided ID. The GTM script is also asynchronously loaded from "https://www.googletagmanager.com/gtm.js?id=GTM-T8JZT2G2". Finally, a video player script sets autoplay, muted, and loop properties for an element with the ID 'ospVideo'.

---
+++ script.js
@@ -0,0 +1,23 @@
+<!-- GA4 -->
+    <script async src=“https://www.googletagmanager.com/gtag/js?id=G-VH41JYZ1B0”></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag(‘js’, new Date());
+    gtag(‘config’, ‘G-VH41JYZ1B0’);
+    </script>
+<!-- GTM -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
+    new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
+    ’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,‘script’,‘dataLayer’,‘GTM-T8JZT2G2’);</script>
+<!-- Video Player -->
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('ospVideo');
+    video.autoplay = true;
+    video.muted = true;
+    video.loop = true;
+});
+</script>
