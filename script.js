7fd7f68d5499afffc927e2e9a58b0375988855d1
Spencer Thayer <me@spencerthayer.com>
2023-09-29 11:00:46 -0700

    qwdqwdqwdqwd
    q

--- script.js
+++ script.js
@@ -12,27 +12,56 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-T8JZT2G2');
 // <!-- Video Player -->
 document.addEventListener('DOMContentLoaded', function() {
+    
     var video = document.getElementById('ospVideo');
-        video.autoplay = true;
-        video.muted = true;
-        video.loop = true;
-        video.play();
-
+    
+    var isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
+    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    var isEdge = navigator.userAgent.indexOf('Edg') > -1;
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    
+    // Assume MP4 source by default and apply styles
+    var applyStyles = false;
+    
+    if(isSafari || isChrome) {
+        video.src = 'https://storage.googleapis.com/osp-video/OSP_apply_the_law-1580x1080_hvec.mp4';
+    } else {
+        // For browsers viewing MP4 source, do not apply styles
+        video.src = 'https://storage.googleapis.com/osp-video/OSP_apply_the_law-1580x1080.mp4';
+        applyStyles = false;
+    }
+    
+    // Apply inline styles to the elements with class '.applythelaw' only if applyStyles is true
+    if(applyStyles) {
         var applyTheLawElements = document.querySelectorAll('.applythelaw');
         applyTheLawElements.forEach(function(element) {
             element.style.backgroundImage = 'url(https://storage.googleapis.com/osp-video/img/applythelaw_bg.png)';
             element.style.backgroundPosition = 'bottom center';
-            element.style.backgroundSize = 'cover';
+            element.style.backgroundSize = 'contain';
         });
-
-        var exceptionalElements = document.querySelectorAll('.exceptional .flexbox');
-        exceptionalElements.forEach(function(element) {
-            element.style.gap = '128px';
-        });
-        
-        var threeColumnElements = document.querySelectorAll('.threecolumn .flexbox');
-        threeColumnElements.forEach(function(element) {
-            element.style.gap = '128px';
-        });
+    }
+    
+    // Apply inline style to body and html
+    document.body.style.overflowX = 'hidden';
+    document.documentElement.style.overflowX = 'hidden'; // html
+    
+    var exceptionalElements = document.querySelectorAll('.exceptional .flexbox');
+    exceptionalElements.forEach(function(element) {
+        element.style.gap = '128px';
     });
+        
+    var threeColumnElements = document.querySelectorAll('.threecolumn .flexbox');
+    threeColumnElements.forEach(function(element) {
+        element.style.gap = '128px';
+    });
+    
+    video.autoplay = true;
+    video.muted = true;
+    video.loop = true;
+    video.load(); // Important: load the video again after changing source
+    video.play();
+});
+
+
 //</script>
